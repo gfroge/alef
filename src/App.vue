e5828a561e8fb86eaa2b9d1db2df0db588576a37
unknown <>
2022-11-23 23:49:40 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <page-header></page-header>
+</template>
+
+<script lang="ts">
+import PageHeader from "@/components/PageHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageHeader,
+  },
+  setup(){
+
+  }
+};
+</script>
+
+<style scoped>
+</style>
