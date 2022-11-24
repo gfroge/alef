e9c0ab11c2065471e3f6007b92edb53e4613913e
unknown <>
2022-11-24 21:25:06 +0300

    0.0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <page-header></page-header>
+  <page-header ></page-header>
+  <clothing-card :cardData="clothingCards[0]"></clothing-card>
 </template>
 
 <script lang="ts">
 import PageHeader from "@/components/PageHeader.vue";
+import ClothingCard from '@/components/UI/ClothingCard.vue';
 
 export default {
   name: "App",
   components: {
-    PageHeader,
+    PageHeader, ClothingCard
   },
-  setup(){
-
-  }
+  setup() {
+    const clothingCards = [
+      {link:'', likes: 7,imagePath:''}
+    ];
+    
+    return{clothingCards}
+  },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/UI/ClothingCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="clothing-card">
+    <picture>
+      <source srcset="">
+      <img :src="props.cardData" alt="фото одежды">
+    </picture>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  cardData: {
+      type: Array,
+      required: true
+    },
+})
+</script>
+
+<style scoped></style>
